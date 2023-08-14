9a2ed9dbe178bc93066742b880bad1857552a94e
Reaper Gelera <ahoy@barelyhuman.dev>
2023-08-14 11:28:41 +0530

    feat: file segregation based types

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,8 @@
+import preact from '@astrojs/preact'
+import tailwind from '@astrojs/tailwind'
+import vercel from '@astrojs/vercel/serverless'
 import { defineConfig } from 'astro/config'
 
-// https://astro.build/config
-import tailwind from '@astrojs/tailwind'
-
-// https://astro.build/config
-import vercel from '@astrojs/vercel/serverless'
-
-// https://astro.build/config
-import preact from '@astrojs/preact'
-
-// https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), preact()],
   output: 'server',
--- src/lib/types.js
+++ src/lib/types.js
@@ -17,11 +17,21 @@ const typeMap = {
   },
 }
 
-export async function readTypescriptFileBuffer(
-  filebuffer,
-  pkgName,
-  { name } = {}
-) {
+export async function collectDeclarations(sourcefile) {
+  let declarations = []
+  const children = []
+  ts.forEachChild(sourcefile, child => {
+    children.push(child)
+  })
+
+  for (let child of children) {
+    const declr = await findTypeDef(child, sourcefile)
+    declarations = declarations.concat(declr)
+  }
+  return declarations
+}
+
+export async function readTypescriptFileBuffer(filebuffer, { name } = {}) {
   let declarations = []
   name = name || 'tmp.d.ts'
   const sourceFile = ts.createSourceFile(name, filebuffer)
@@ -31,39 +41,19 @@ export async function readTypescriptFileBuffer(
   })
 
   for (let child of children) {
-    const declr = await findTypeDef(child, sourceFile, pkgName)
+    const declr = await findTypeDef(child, sourceFile)
     declarations = declarations.concat(declr)
   }
   return declarations
 }
 
-export async function findTypeDef(child, sourceFile, pkgName) {
+export async function findTypeDef(child, sourceFile) {
   const printer = ts.createPrinter({ newLine: ts.NewLineKind.LineFeed })
 
   const declarations = []
   const _internalType = Object.values(typeMap).find(x => x.kind === child.kind)
 
   switch (child.kind) {
-    case ts.SyntaxKind.ExportDeclaration: {
-      // relative import
-      if (child.moduleSpecifier?.text?.startsWith('./')) {
-        const modName = child.moduleSpecifier?.text
-        const fetchableName = modName.replace(extname(modName), '')
-        const defs = await fetchTypeDefinitions(
-          pkgName,
-          fetchableName + '.d.ts'
-        ).catch(err => false)
-        if (!defs) {
-          break
-        }
-        const declrs = await readTypescriptFileBuffer(defs, pkgName, {
-          name: `${modName}.d.ts`,
-        })
-        declarations.push(...declrs)
-      }
-      break
-    }
-
     case ts.SyntaxKind.VariableStatement: {
       ts.forEachChild(child.declarationList, declaration => {
         if (declaration?.type?.kind === ts.SyntaxKind.FunctionType) {
@@ -91,4 +81,53 @@ export async function findTypeDef(child, sourceFile, pkgName) {
   }
 
   return declarations
+}
+
+export async function getTypeDefFiles(entryTypeDef, pkgName) {
+  // entry type def file
+  const entrySourceFile = ts.createSourceFile('index.d.ts', entryTypeDef)
+  const relativeFiles = []
+
+  relativeFiles.push({
+    name: 'index.d.ts',
+    typeDefs: entryTypeDef,
+    sourceFile: entrySourceFile,
+  })
+
+  const children = []
+  ts.forEachChild(entrySourceFile, child => {
+    children.push(child)
+  })
+
+  for (let child of children) {
+    if (child.kind !== ts.SyntaxKind.ExportDeclaration) continue
+
+    // Handle relative exports
+    // ./another-module
+    if (child.moduleSpecifier?.text?.startsWith('./')) {
+      const modName = child.moduleSpecifier?.text
+      const fetchableName = modName.replace(extname(modName), '') + '.d.ts'
+      const defs = await fetchTypeDefinitions(pkgName, fetchableName).catch(
+        err => false
+      )
+      if (!defs) {
+        continue
+      }
+
+      relativeFiles.push({
+        name: fetchableName,
+        typeDefs: defs,
+        sourceFile: ts.createSourceFile(fetchableName, defs),
+      })
+    }
+
+    // TODO: handle aliased exports
+    // pkgName/another-module
+    // can simple re-do the package fetching sequence but would like to
+    // avoid re-fetching details for the same package again
+    // Need to move the logic from the [...pkgName] route to a helper
+    // function first
+  }
+
+  return relativeFiles
 }
